5230fb8925c3a5f86fdcd21ff4c36aae92c0ca6d
Ross-Morgan <morgan.ross.512@gmail.com>
2025-09-16 04:18:37 +0100

    remove proc_macro2 as a dependency

--- chemistru-elements-macro/src/lib.rs
+++ chemistru-elements-macro/src/lib.rs
@@ -1,15 +1,15 @@
 extern crate proc_macro;
 
-use chemistru_elements_inner::{Element, ELEMENTS};
+use chemistru_elements_inner::{ELEMENTS, Element};
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 
 #[proc_macro]
 pub fn generate_elements_consts(_: TokenStream) -> TokenStream {
     let elements = ELEMENTS.iter();
 
-    let mut stream = TokenStream2::new();
+    // let mut stream = TokenStream2::new();
+    let mut stream = TokenStream::new();
 
     for element in elements {
         stream.extend(generate_const_init(element));
@@ -18,10 +18,10 @@ pub fn generate_elements_consts(_: TokenStream) -> TokenStream {
     TokenStream::from(stream)
 }
 
-fn generate_const_init(element: &Element) -> TokenStream2 {
+fn generate_const_init(element: &Element) -> TokenStream {
     let assignment_name = element.name().to_uppercase();
 
-    quote! {
+    TokenStream::from(quote! {
         const #assignment_name: ::chemistru_elements::Element = #element;
-    }
+    })
 }
