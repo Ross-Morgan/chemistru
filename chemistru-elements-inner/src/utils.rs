53fccaac7c09a58dab42221693d23c758f674146
Ross-Morgan <morgan.ross.512@gmail.com>
2025-09-17 02:09:26 +0100

    update docs

--- chemistru-elements-inner/src/utils.rs
+++ chemistru-elements-inner/src/utils.rs
@@ -1,12 +1,16 @@
 use crate::{ELEMENTS, Element};
 
-/// Returns element with the given atomic (proton) number
+// This import allows docs to link to it without having to fully qualify path
+#[allow(unused_imports)]
+use std::sync::LazyLock;
+
+/// Returns [`Element`] with the given atomic (proton) number
 #[must_use]
 pub fn element_from_atomic_number(n: u8) -> Option<&'static Element> {
     ELEMENTS.get(n as usize - 1)
 }
 
-/// Returns element with the given name
+/// Returns [`Element`] with the given name
 ///
 /// Names are case-insensitive and several spellings are accepted for the following:
 /// - Caesium, Cesium
@@ -30,7 +34,7 @@ pub fn element_from_name(name: &str) -> Option<&'static Element> {
         .find(|n| n.name().to_lowercase() == name.to_lowercase())
 }
 
-/// Initialises the `LazyLock`-wrapped static vector of [`Element`]s
+/// Initialises the [`LazyLock`]-wrapped static vector of [`Element`]s
 pub fn preload_elements() {
     // Get first
     let _ = std::hint::black_box(ELEMENTS.first());
